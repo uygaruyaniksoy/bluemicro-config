586df840ddbda63eb01db225ad0be1772186a81c
Uygar Uyaniksoy <uygaruyaniksoy@gmail.com>
2026-05-13 23:47:32 +0300

    Update pins

--- firmware/hardware_config.h
+++ firmware/hardware_config.h
@@ -5,7 +5,7 @@
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 7
 #define MATRIX_ROW_PINS { 2, 31, 29, 20, 13, 8, 6, 22, 15, 24 }
-#define MATRIX_COL_PINS { 9, 38, 36, 11, 32, 43, 10 }
+#define MATRIX_COL_PINS { 9, 36, 11, 32, 43, 10, 38 }
 
 #define ARDUINO_NICE_NANO 1
 #define UNUSED_PINS {}
